44ab41ee4bd7c307734ccafab459d49cbce7cbfc
tienhai488 <97999852+tienhai488@users.noreply.github.com>
2023-01-31 15:44:50 +0700

    migrate

---
+++ database/migrations/2023_01_30_204915_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');//int,auto increment,primary key 
+            $table->string('name',200);//varchar(200)
+            $table->text('description')->nullable();
+            $table->timestamps();//created_at,updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
