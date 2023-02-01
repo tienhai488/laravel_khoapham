a0e32e1d3a083215bf00adc58880b9141a0444eb
tienhai488 <97999852+tienhai488@users.noreply.github.com>
2023-02-01 15:06:32 +0700

    confirm pass

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ConfirmsPasswords;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
 
 class ConfirmPasswordController extends Controller
 {
@@ -15,5 +19,39 @@ class ConfirmPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+    }
+
+    protected function rules()
+    {
+        return [
+            'password' => 'required|current_password:web',
+        ];
+    }
+
+    protected function validationErrorMessages()
+    {
+        return [
+            'password.required'=>'Vui lòng nhập mật khẩu!',
+            'password.current_password'=>'Mật khẩu vừa nhập không chính xác!',
+        ];
+    }
+
+    public function confirm(Request $request)
+    {
+        $request->validate($this->rules(), $this->validationErrorMessages());
+
+        $this->resetPasswordConfirmationTimeout($request);
+
+        // // Xử lý gửi email
+        // Mail::send([],[],function ($message){
+        //     $message->to('tienhai488@gmail.com')
+        //     ->subject('Xac thuc mat khau')
+        //     ->getBody('Ban da xac thuc mat khau thanh cong!');
+        // });
+
+
+        return $request->wantsJson()
+                    ? new JsonResponse([], 204)
+                    : redirect()->intended($this->redirectPath());
     }
 }
